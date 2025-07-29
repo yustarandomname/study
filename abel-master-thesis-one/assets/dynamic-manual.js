4469569c5dfeb5afde1042f35310679e49e2cc1c
yustarandomname <yustarandomname@users.noreply.github.com>
2025-07-29 14:27:27 +0000

    deploy: fa6698d45af56174b24add1bed7b58d2737a8f80

---
+++ abel-master-thesis-one/assets/dynamic-manual.js
@@ -0,0 +1,13 @@
+// import { WebsiteComponent } from '../../../src/parser/types';
+
+/**
+ * @type WebsiteComponent
+ */
+export default {
+  type: 'website',
+  path: 'https://example.com',
+  parameters: {
+    title: 'Example Website',
+  },
+  response: [],
+};
